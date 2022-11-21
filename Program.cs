a4b5d568c45d53564d7e62b52a1b14b43518cd53
James Rebello <Jikrebello.za@gmail.com>
2022-11-21 23:34:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Bluerain_Tech_Introduction;
+
+var map = new Map(10, 10);
+
+map.AddRoverToMap(startingX: 0, startingY: 0, destinationX: 2, destinationY: 7);
+map.AddRoverToMap(startingX: 8, startingY: 8, destinationX: 7, destinationY: 2);
+map.MoveRover(0, "NNNEESSEENNNNNN");
+map.MoveRover(1, "SSWWWWWW");
+
+map.RenderWorld();
+
+Console.ReadLine();
---
+++ Rover.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Bluerain_Tech_Introduction
+{
+    public class Rover
+    {
+        public Vector2 StartingPosition { get; set; }
+        public Vector2 CurrentPosition { get; set; }
+        public Vector2 TargetDestination { get; set; }
+    }
+}
